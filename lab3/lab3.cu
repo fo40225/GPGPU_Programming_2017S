0586aa12f54bd382bc03711c3802eb9d12d0f7c0
fo40225 <fo40225@hotmail.com>
2017-05-20 14:25:02 +0800

    hw3 init

---
+++ lab3/lab3.cu
@@ -0,0 +1,59 @@
+#if defined  __INTELLISENSE__ || defined  __RESHARPER__
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#endif
+
+#ifdef __CUDACC__
+#define KERNEL_ARGS2(grid, block) <<< grid, block >>>
+#define KERNEL_ARGS3(grid, block, sh_mem) <<< grid, block, sh_mem >>>
+#define KERNEL_ARGS4(grid, block, sh_mem, stream) <<< grid, block, sh_mem, stream >>>
+#else
+#define KERNEL_ARGS2(grid, block)
+#define KERNEL_ARGS3(grid, block, sh_mem)
+#define KERNEL_ARGS4(grid, block, sh_mem, stream)
+#endif
+
+#include "lab3.h"
+#include <cstdio>
+
+__device__ __host__ int CeilDiv(int a, int b) { return (a-1)/b + 1; }
+__device__ __host__ int CeilAlign(int a, int b) { return CeilDiv(a, b) * b; }
+
+__global__ void SimpleClone(
+	const float *background,
+	const float *target,
+	const float *mask,
+	float *output,
+	const int wb, const int hb, const int wt, const int ht,
+	const int oy, const int ox
+)
+{
+	const int yt = blockIdx.y * blockDim.y + threadIdx.y;
+	const int xt = blockIdx.x * blockDim.x + threadIdx.x;
+	const int curt = wt*yt+xt;
+	if (yt < ht && xt < wt && mask[curt] > 127.0f) {
+		const int yb = oy+yt, xb = ox+xt;
+		const int curb = wb*yb+xb;
+		if (0 <= yb && yb < hb && 0 <= xb && xb < wb) {
+			output[curb*3+0] = target[curt*3+0];
+			output[curb*3+1] = target[curt*3+1];
+			output[curb*3+2] = target[curt*3+2];
+		}
+	}
+}
+
+void PoissonImageCloning(
+	const float *background,
+	const float *target,
+	const float *mask,
+	float *output,
+	const int wb, const int hb, const int wt, const int ht,
+	const int oy, const int ox
+)
+{
+	cudaMemcpy(output, background, wb*hb*sizeof(float)*3, cudaMemcpyDeviceToDevice);
+	SimpleClone KERNEL_ARGS2(dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16))(
+		background, target, mask, output,
+		wb, hb, wt, ht, oy, ox
+	);
+}
